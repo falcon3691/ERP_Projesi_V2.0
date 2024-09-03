62b8f9bf0d924d7b373650d5a4a6a78d8d65f1b4
Oguzhan <Oguzhan@DESKTOP-HSH38D0>
2024-09-03 18:05:41 +0300

    muhasebe ekranının işlemleri ayarlandı.

--- ERP_Projesi_V2.0/Ekranlar/Modüller/muhasebe.Designer.cs
+++ ERP_Projesi_V2.0/Ekranlar/Modüller/muhasebe.Designer.cs
@@ -39,6 +39,8 @@ namespace ERP_Projesi_V2._0.Ekranlar.Modüller
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,11 +63,11 @@ namespace ERP_Projesi_V2._0.Ekranlar.Modüller
             this.label3.Size = new System.Drawing.Size(81, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Son Tarih";
-            this.label3.Visible = false;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Enabled = false;
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(16, 32);
             this.dateTimePicker1.Name = "dateTimePicker1";
@@ -75,12 +77,12 @@ namespace ERP_Projesi_V2._0.Ekranlar.Modüller
             // dateTimePicker2
             // 
             this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker2.Enabled = false;
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker2.Location = new System.Drawing.Point(139, 32);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(110, 22);
             this.dateTimePicker2.TabIndex = 4;
-            this.dateTimePicker2.Visible = false;
             // 
             // checkBox1
             // 
@@ -91,62 +93,92 @@ namespace ERP_Projesi_V2._0.Ekranlar.Modüller
             this.checkBox1.TabIndex = 5;
             this.checkBox1.Text = "Tarih aralığı seç";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button1.Location = new System.Drawing.Point(286, 9);
+            this.button1.Location = new System.Drawing.Point(255, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 35);
             this.button1.TabIndex = 6;
             this.button1.Text = "İşlemleri Listele";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button2.Location = new System.Drawing.Point(423, 10);
+            this.button2.Location = new System.Drawing.Point(392, 5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(131, 35);
             this.button2.TabIndex = 7;
             this.button2.Text = "Alacakları Listele";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button3.Location = new System.Drawing.Point(423, 51);
+            this.button3.Location = new System.Drawing.Point(392, 46);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(131, 35);
             this.button3.TabIndex = 8;
             this.button3.Text = "Borçları Listele";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(16, 87);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(656, 351);
+            this.dataGridView1.Size = new System.Drawing.Size(478, 351);
             this.dataGridView1.TabIndex = 9;
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button4.Location = new System.Drawing.Point(286, 51);
+            this.button4.Location = new System.Drawing.Point(255, 46);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(131, 35);
             this.button4.TabIndex = 10;
             this.button4.Text = "Listeyi Raporla";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button5.Location = new System.Drawing.Point(529, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 35);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Girdileri Listele";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button6.Location = new System.Drawing.Point(529, 45);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(131, 35);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Çıktıları Listele";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // muhasebe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 450);
+            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
@@ -177,5 +209,7 @@ namespace ERP_Projesi_V2._0.Ekranlar.Modüller
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- ERP_Projesi_V2.0/Ekranlar/Modüller/muhasebe.cs
+++ ERP_Projesi_V2.0/Ekranlar/Modüller/muhasebe.cs
@@ -7,15 +7,288 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace ERP_Projesi_V2._0.Ekranlar.Modüller
 {
     public partial class muhasebe : Form
     {
+        //SQL veri tabanı bağlantı kodu.
+        public String baglantiKodu = "Data Source=DESKTOP-HSH38D0\\SQLEXPRESS;Initial Catalog=KKP_V2;Integrated Security=True";
+
+        //Muhasebe değişkenleri başlangıcı.
+        public DateTime tarih;
+        public int toplamFiyat;
+        public String girdiCikti;
+        public String islemTuru;
+        //Muhasebe değişkenleri sonu.
+
         public muhasebe()
         {
             InitializeComponent();
+            listele();
+            AdjustDataGridViewSize();
         }
 
+        private void AdjustDataGridViewSize()
+        {
+            int totalWidth = 20;
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                totalWidth += column.Width;
+            }
+
+            dataGridView1.Width = totalWidth;
+        }
+        //İşlemleri Listele butonu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        //Borçları Listele butonu
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listele("islemTuru", "BORÇ");
+        }
+
+        //Alacakları Listele butonu
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listele("islemTuru", "ALACAK");
+        }
+        
+        //Girdileri Listele butonu
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listele("girdiCikti", "GİRDİ");
+        }
+
+        //Çıktıları Listele butonu
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listele("girdiCikti", "ÇIKTI");
+        }
+
+        //Listeyi Raporla butonu
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Varsayılan dosya yolu ve adı ayarlama
+            string masaustuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string varsayilanDosyaAdi = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".pdf";
+
+            saveFileDialog.InitialDirectory = masaustuYolu;
+            saveFileDialog.FileName = varsayilanDosyaAdi;
+            saveFileDialog.Filter = "PDF Dosyaları (*.pdf)|*.pdf|Tüm Dosyalar (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = saveFileDialog.FileName;
+
+                try
+                {
+                    //ExportDataGridViewToPdf(dataGridView1, dosyaYolu);
+                    CreatePdf(dosyaYolu, dataGridView1);
+                    MessageBox.Show("Dosya başarıyla kaydedildi: " + dosyaYolu);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        //checkBox1'in değerine göre, dateTimePicker2'nin Visible değerini değiştirir.
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                dateTimePicker1.Enabled = true;
+                dateTimePicker2.Enabled = true;
+            }
+            else
+            {
+                dateTimePicker1.Enabled = false;
+                dateTimePicker2.Enabled = false;
+            }
+        }
+        public void degiskenleriAta()
+        {
+            tarih = DateTime.Today.AddDays(3);
+            toplamFiyat = 300;
+            girdiCikti = "ÇIKTI";
+            islemTuru = "BORÇ";
+        }
+
+        public void degerEkle(DateTime tarih, int toplamFiyat, String girdiCikti, String islemTuru)
+        {
+            degiskenleriAta();
+            using (SqlConnection baglanti = new SqlConnection(baglantiKodu))
+            {
+                String sqlKomutu = "INSERT INTO muhasebe(islemTarihi, toplamFiyat, girdiCikti, islemTuru) " +
+                                                 "VALUES(@islemTarihi, @toplamFiyat, @girdiCikti, @islemTuru)";
+                using (SqlCommand komut = new SqlCommand(sqlKomutu, baglanti))
+                {
+                    komut.Parameters.Add("@islemTarihi", SqlDbType.DateTime).Value = tarih;
+                    komut.Parameters.Add("@toplamFiyat", SqlDbType.Int).Value = toplamFiyat;
+                    komut.Parameters.Add("@girdiCikti", SqlDbType.NVarChar).Value = girdiCikti;
+                    komut.Parameters.Add("@islemTuru", SqlDbType.NVarChar).Value = islemTuru;
+                    try
+                    {
+                        baglanti.Open();
+                        int sonuc = komut.ExecuteNonQuery();
+                        if (sonuc == 1)
+                        {
+                            Console.Out.WriteLine("İşlem eklendi.");
+                            listele();
+                        }
+                        else
+                            Console.Out.WriteLine("İşlem eklenemedi.");
+                    }
+                    catch (Exception hata)
+                    {
+                        MessageBox.Show(hata.ToString());
+                    }
+                    finally
+                    {
+                        baglanti.Close();
+                    }
+                }
+            }
+        }
+
+        public void listele(String sutun = null, String deger = null)
+        {
+            using (SqlConnection baglanti = new SqlConnection(baglantiKodu))
+            {
+                String sqlKomutu = "SELECT * FROM muhasebe";
+                if (sutun != null && deger != null)
+                {
+                    sqlKomutu += $" WHERE {sutun} = @deger";
+                    if(checkBox1.Checked)
+                        sqlKomutu += " AND islemTarihi >= @deger1 AND islemTarihi <= @deger2";
+                }
+                else if(checkBox1.Checked)
+                    sqlKomutu += " WHERE islemTarihi >= @deger1 AND islemTarihi <= @deger2";
+
+                try
+                {
+                    baglanti.Open();
+                    SqlCommand komut = new SqlCommand(sqlKomutu, baglanti);
+                    if (sutun != null && deger != null)
+                        komut.Parameters.Add("@deger", SqlDbType.NVarChar).Value = deger;
+                    if (checkBox1.Checked)
+                    {
+                        komut.Parameters.Add("@deger1", SqlDbType.DateTime).Value = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+                        komut.Parameters.Add("@deger2", SqlDbType.DateTime).Value = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(komut);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    baglanti.Close();
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show(hata.ToString());
+                }
+                finally
+                {
+                    baglanti.Close();
+                }
+
+            }
+        }
+
+        //dataGridView'ın içeriğini bir PDF dosyasına yazar, kullanıcıdan dosya yolu ve dosya adını belirtebileceği bir ekran açılır.
+        //Eğer dosya yolu veya dosya adı değiştirilmezse, varsayılan olarak dosya yolu masaüstü ve dosya adı "gününTarihi_Saat" olacak
+        //şekilde ayarlanır. 
+        //Oluşturulan dosya kayıt edilir.
+        public void CreatePdf(string filePath, DataGridView dataGridView)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("File path cannot be null or empty.");
+            }
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                using (Document document = new Document(PageSize.A4))
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                    writer.PageEvent = new PdfPageEvents();
+
+                    document.Open();
+
+                    // Tarih bilgisini ekle
+                    string todayDate = DateTime.Now.ToString("dd.MM.yyyy"); 
+                    BaseFont bfArialUniCode = BaseFont.CreateFont("C:\\Windows\\Fonts\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                    iTextSharp.text.Font font = new iTextSharp.text.Font(bfArialUniCode, 12, iTextSharp.text.Font.BOLD);
+                    Paragraph dateParagraph = new Paragraph(todayDate + "\n\n", font);
+                    dateParagraph.Alignment = Element.ALIGN_LEFT;
+                    document.Add(dateParagraph);
+
+                    // Sütun isimlerini ekle
+                    PdfPTable table = new PdfPTable(dataGridView.ColumnCount);
+                    table.WidthPercentage = 100;
+
+                    foreach (DataGridViewColumn column in dataGridView.Columns)
+                    {
+                        PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText, font));
+                        cell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                        table.AddCell(cell);
+                    }
+
+                    // Çizgi ekle
+                    table.HeaderRows = 1;
+                    document.Add(table);
+
+                    // Satırları ekle
+                    foreach (DataGridViewRow row in dataGridView.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                PdfPCell pdfCell = new PdfPCell(new Phrase(cell.Value?.ToString() ?? string.Empty, font));
+                                table.AddCell(pdfCell);
+                            }
+                        }
+                    }
+
+                    // Son çizgi ekle
+                    document.Add(table);
+                    document.Add(new Paragraph("\n")); // Boşluk bırak
+
+                    document.Close();
+                }
+            }
+        }
+
+    }
+    //Oluşturulan PDF dosyasında her sayfanın sonuna sayfa sayısını yazdırmak için kullanılır.
+    public class PdfPageEvents : PdfPageEventHelper
+    {
+        private int pageNumber = 1;
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            base.OnEndPage(writer, document);
+
+            // Sayfa numarasını yazmak için PdfContentByte kullanımı
+            PdfContentByte canvas = writer.DirectContent;
+            Phrase phrase = new Phrase($"Sayfa: {pageNumber}", FontFactory.GetFont("Helvetica", 10, iTextSharp.text.Font.BOLD));
+            ColumnText.ShowTextAligned(canvas, Element.ALIGN_RIGHT, phrase, document.PageSize.Width - 40, 30, 0);
+
+            // Sayfa numarasını arttır
+            pageNumber++;
+        }
     }
 }
